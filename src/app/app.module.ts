f05c182d2ab54365e5b60bb636cf8c712f777784
Himanshu Jain <himanshujain2121@gmail.com>
2019-09-02 15:32:11 +0530

    add side and aside

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TokeninterceptorService } from './auth/tokeninterceptor.service';
 import { LoginService } from './service/login.service';
+import { HeaderComponent } from './component/header/header.component';
+import { SideBarComponent } from './component/side-bar/side-bar.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { MainContentComponent } from './component/main-content/main-content.component';
+import { AsideBarComponent } from './component/aside-bar/aside-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideBarComponent,
+    FooterComponent,
+    MainContentComponent,
+    AsideBarComponent
   ],
   imports: [
     BrowserModule,
