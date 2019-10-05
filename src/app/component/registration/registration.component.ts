32578cd18ba28ecbc91d7c50393f2b4c17d09761
Himanshu Jain <himanshujain2121@gmail.com>
2019-10-05 23:38:26 +0530

    Submitting form data

--- src/app/component/registration/registration.component.ts
+++ src/app/component/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../model/user';
+import { EnrolmentService } from '../../service/enrolment/enrolment.service';
 
 @Component({
   selector: 'app-registration',
@@ -8,22 +9,33 @@ import { User } from '../../model/user';
 })
 export class RegistrationComponent implements OnInit {
 
-  topics =['Angular','React','Vue']; 
-  userModel = new User('jim','namrata@gmail.com','5426321456','default','morning',true);
+  topics = ['Angular', 'React', 'Vue'];
+  userModel = new User('jim', 'namrata@gmail.com', '5426321456', 'default', 'morning', true);
 
   topicHasError = true;
 
-  validateTopic(value){
-    if(value === 'default'){
-      this.topicHasError = true;
-    }
-    else{
-      this.topicHasError = false;
-    }
-  }
-  constructor() { }
+  constructor(private enrollService: EnrolmentService) { }
 
   ngOnInit() {
   }
+
+  validateTopic(value) {
+    if (value === 'default') {
+      this.topicHasError = true;
+    }
+    else {
+      this.topicHasError = false;
+    }
+  }
+
+  onSubmit() {
+    // console.log(this.userModel);
+    this.enrollService.enroll(this.userModel)
+      .subscribe(
+        data => console.log('success', data),
+        err => console.log('Failed', err)
+      )
+  }
+
 
 }
