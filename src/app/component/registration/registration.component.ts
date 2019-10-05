0997a6c6e493ada650cea9df9f641fac247522ef
Himanshu Jain <himanshujain2121@gmail.com>
2019-10-05 20:40:02 +0530

    create register form

--- src/app/component/registration/registration.component.ts
+++ src/app/component/registration/registration.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
+  topics =['Angular','React','Vue']; 
   constructor() { }
 
   ngOnInit() {
